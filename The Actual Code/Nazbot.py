e90024e3f09c24628cbc41db21d13031a580129a
Oresfall <putraadhitama502@gmail.com>
2024-01-17 00:43:34 +0700

    Added basic code for discord bot

--- The Actual Code/Nazbot.py
+++ The Actual Code/Nazbot.py
@@ -2,9 +2,25 @@
 # Plans:
 #   1. Adding commands that send nazrin fan art from Reddit
 #   2. Nazrin trivias would be funny
-#
+#   3. Cheese images for no reason
 #   
-# 
+#   
 
-if __name__ == 'main':
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="n!", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Cheesed to meet you devs!")
+    print("--------------------------")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Cheesed to meet you, im Nazbot!")
+
+client.run('xxx')
+
+if __name__ == '__main__':
     print("nazrin!")
